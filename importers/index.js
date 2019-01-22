8691716fd1f87b3608cf92aa11c0965ac722fe7c
Kristofor Carle <kris@maphubs.com>
2019-01-22 14:33:40 -0500

    remove lodash dependency

--- importers/index.js
+++ importers/index.js
@@ -1,11 +1,16 @@
 // @flow
 const csv = require('./csv')
+console.log('loaded CSV importer')
 const shapefile = require('./shapefile')
+console.log('loaded Shapefile')
 const kml = require('./kml')
+console.log('loaded KML importer')
 const geojson = require('./geojson')
+console.log('loaded GeoJSON importer')
 const gpx = require('./gpx')
+console.log('loaded GPX importer')
 const geobuf = require('./geobuf')
-const _endsWith = require('lodash.endswith')
+console.log('loaded GeoBuf importer')
 const debug = require('../debug')('importers')
 
 module.exports = {
@@ -16,28 +21,28 @@ module.exports = {
   gpx,
   geobuf,
   getImporterFromFileName (fileName: string) {
-    if (_endsWith(fileName, '.zip')) {
+    if (fileName.endsWith('.zip')) {
       debug.log('Zip File Detected')
       return shapefile
-    } else if (_endsWith(fileName, '.pbf')) {
+    } else if (fileName.endsWith('.pbf')) {
       debug.log('Geobuf File Detected')
       return geobuf
-    } else if (_endsWith(fileName, '.maphubs')) {
+    } else if (fileName.endsWith('.maphubs')) {
       debug.log('MapHubs File Detected')
       return geobuf
-    } else if (_endsWith(fileName, '.csv')) {
+    } else if (fileName.endsWith('.csv')) {
       debug.log('CSV File Detected')
       return csv
-    } else if (_endsWith(fileName, '.kml')) {
+    } else if (fileName.endsWith('.kml')) {
       debug.log('KML File Detected')
       return kml
-    } else if (_endsWith(fileName, '.gpx')) {
+    } else if (fileName.endsWith('.gpx')) {
       debug.log('GPX File Detected')
       return gpx
-    } else if (_endsWith(fileName, '.geojson') || _endsWith(fileName, '.json')) {
+    } else if (fileName.endsWith('.geojson') || fileName.endsWith('.json')) {
       debug.log('GeoJSON File Detected')
       return geojson
-    } else if (_endsWith(fileName, '.shp')) {
+    } else if (fileName.endsWith('.shp')) {
       throw new Error('Shapefile must uploaded in a Zip file')
     } else {
       throw new Error(`Unsupported file type: ${fileName}`)
