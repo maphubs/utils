996c0dfae56dd31e7299bcfd31d1e02754cb211b
Kristofor Carle <kris@maphubs.com>
2019-12-03 18:00:12 -0500

    remove importer mapbox/shapefile-fairy dependencies, use ogr2ogr directly

--- importers/shapefile/index.js
+++ importers/shapefile/index.js
@@ -19,57 +19,34 @@ module.exports = async function (filePath: string, layer_id: number, config?: Ob
   /* eslint-disable security/detect-non-literal-fs-filename */
   // file path is a folder from a env var + a GUID, not orginal filename
   log.info('Starting Shapefile Importer')
-  const pipedStream = fs.createReadStream(filePath).pipe(unzip.Extract({path: filePath + '_zip'}))
-  await streamCloseToPromise(pipedStream)
-  log.info('Zip file extracted')
-  // validate
-  log.info('Validating Shapefile')
-  const result = await shapefileFairy(filePath, {extract: false})
-  debug.log('ShapefileFairy Result: ' + JSON.stringify(result))
-  if (result && result.code === 'MULTIPLESHP') {
-    log.info('Multiple Shapfiles Detected: ' + result.shapefiles.toString())
-    // tell the client if we were successful
-    return {
-      success: false,
-      code: result.code,
-      shapefiles: result.shapefiles
-    }
-  } else if (result) {
-    debug.log('Shapefile Validation Successful')
-    const shpFilePath = filePath + '_zip/' + result.shp
-    debug.log('shapefile: ' + shpFilePath)
-    const ogr = ogr2ogr(shpFilePath)
-      .format('GeoJSON')
-      .skipfailures()
-      .options(['-t_srs', 'EPSG:4326'])
-      .timeout(1200000)
-      .stream()
 
-    return new Promise((resolve, reject) => {
-      const stream = ogr.pipe(JSONStream.parse('features.*'))
-      const features = []
+  const shpFilePath = filePath + '_zip/' + result.shp
+  debug.log('shapefile: ' + filePath)
+  const ogr = ogr2ogr(filePath)
+    .format('GeoJSON')
+    .skipfailures()
+    .options(['-t_srs', 'EPSG:4326'])
+    .timeout(1200000)
+    .stream()
 
-      stream.on('data', (data) => {
-        features.push(data)
-      })
+  return new Promise((resolve, reject) => {
+    const stream = ogr.pipe(JSONStream.parse('features.*'))
+    const features = []
 
-      stream.on('end', () => {
-        debug.log('Shapefile Conversion to GeoJSON Successful')
-        log.info(`Converted ${features.length} features to GeoJSON`)
-        resolve({
-          type: 'FeatureCollection',
-          features
-        })
-      })
-      stream.on('error', (err) => {
-        throw err
+    stream.on('data', (data) => {
+      features.push(data)
+    })
+
+    stream.on('end', () => {
+      debug.log('Shapefile Conversion to GeoJSON Successful')
+      log.info(`Converted ${features.length} features to GeoJSON`)
+      resolve({
+        type: 'FeatureCollection',
+        features
       })
     })
-  } else {
-    log.error(`Unknown Shapefile Validation Error`)
-    return {
-      success: false,
-      value: result
-    }
-  }
+    stream.on('error', (err) => {
+      reject(err)
+    })
+  })
 }
